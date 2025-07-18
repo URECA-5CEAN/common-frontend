39946b445888cfc47068227bd223f2156f64905e
leeemingyu <skystar9927@naver.com>
2025-07-18 17:42:10 +0900

    feat : 내 정보 카드 구현 #6

---
+++ src/domains/MyPage/components/profile/UserProfile.tsx
@@ -0,0 +1,46 @@
+import UserInfo from '@/domains/MyPage/components/profile/UserInfo';
+import UserStats from '@/domains/MyPage/components/profile/UserStats';
+import type { UserInfo as UserInfoType } from '@/domains/MyPage/types/profile';
+
+interface ProfileImageProps {
+  src: string;
+  alt: string;
+}
+
+const ProfileImage: React.FC<ProfileImageProps> = ({ src, alt }) => (
+  <div className="w-[144px] h-[163px] overflow-hidden">
+    <img
+      src={src}
+      alt={alt}
+      className="h-full w-auto object-cover object-center"
+    />
+  </div>
+);
+
+interface UserProfileProps {
+  userInfo: UserInfoType;
+  selectedBadgeName: string;
+  onBadgeClick: () => void;
+  profileImageSrc: string;
+}
+
+const UserProfile: React.FC<UserProfileProps> = ({
+  userInfo,
+  selectedBadgeName,
+  onBadgeClick,
+  profileImageSrc,
+}) => (
+  <div className="w-full border border-gray-200 rounded-2xl flex p-5 justify-between">
+    <div className="flex gap-4 items-center">
+      <ProfileImage src={profileImageSrc} alt="우수아이콘" />
+      <UserInfo
+        userInfo={userInfo}
+        selectedBadgeName={selectedBadgeName}
+        onBadgeClick={onBadgeClick}
+      />
+    </div>
+    <UserStats userInfo={userInfo} />
+  </div>
+);
+
+export default UserProfile;
