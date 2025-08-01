ed60ac343ca54f2509a89908e3c6035848bd79bd
leeemingyu <skystar9927@naver.com>
2025-08-02 04:04:59 +0900

    feat : 혜택도감 애니메이션 #158

--- src/domains/MyPage/components/ProgressBar.tsx
+++ src/domains/MyPage/components/ProgressBar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 interface ProgressBarProps {
   current: number;
   max: number;
@@ -5,14 +7,23 @@ interface ProgressBarProps {
 
 export const ProgressBar: React.FC<ProgressBarProps> = ({ current, max }) => {
   const progressPercentage = (current / max) * 100;
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWidth(progressPercentage);
+    });
+
+    return () => clearTimeout(timer);
+  }, [progressPercentage]);
 
   return (
     <div className="relative bg-gray-300 rounded-full h-4 text-xs w-full overflow-hidden">
       <div
         className="absolute top-0 bg-[#96E0ED] h-full rounded-full 
                    after:content-[''] after:block after:h-[2px] after:absolute after:mx-2/3
-                   after:top-1 after:bg-white/30 after:rounded-full after:left-1 after:right-1"
-        style={{ width: `${progressPercentage}%` }}
+                   after:top-1 after:bg-white/30 after:rounded-full after:left-1 after:right-1 transition-all duration-700"
+        style={{ width: `${width}%` }}
       />
       <p className="relative z-1 flex justify-center items-center font-bold text-gray-600">
         {current}/{max}
